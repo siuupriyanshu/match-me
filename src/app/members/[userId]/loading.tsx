2b548cbb235059f4ab0874ff272f67d802b3f134
siuupriyanshu <mandalpriyanshu22@gmail.com>
2024-12-03 17:53:30 +0545

    loading indicators

---
+++ src/app/members/[userId]/loading.tsx
@@ -0,0 +1,12 @@
+import { Spinner } from "@nextui-org/react";
+import React from "react";
+export default function Loading() {
+  return (
+    <div className="flex justify-center items-center vertical-center">
+      <Spinner
+        label="Loading..."
+        color="default"
+      />
+    </div>
+  );
+}
