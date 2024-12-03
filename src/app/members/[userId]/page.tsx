1aeaffa5526e2ec701a2eba21bbf2bf6f4824ba5
siuupriyanshu <mandalpriyanshu22@gmail.com>
2024-12-03 13:55:19 +0545

    server action to get member detail

---
+++ src/app/members/[userId]/page.tsx
@@ -0,0 +1,31 @@
+import { getMemberByUserId } from "@/app/actions/memberActions";
+import {
+    Card,
+  CardBody,
+  CardHeader,
+  Divider,
+} from "@nextui-org/react";
+import { notFound } from "next/navigation";
+import React from "react";
+
+export default async function MemberDetailedPage({
+  params,
+}: {
+  params: { userId: string };
+}) {
+  const member = await getMemberByUserId(
+    params.userId
+  );
+
+  if (!member) return notFound();
+
+  return (
+    <Card>
+      <CardHeader className="text-2xl font-semibold text-default">
+        Profile
+      </CardHeader>
+      <Divider />
+      <CardBody>{member.description}</CardBody>
+    </Card>
+  );
+}
