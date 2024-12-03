c7ac486b70e179abe20d714ba6a02a492a9945f7
siuupriyanshu <mandalpriyanshu22@gmail.com>
2024-12-03 17:53:10 +0545

    member detail chat page

---
+++ src/app/members/[userId]/chat/page.tsx
@@ -0,0 +1,18 @@
+import {
+    CardHeader,
+    Divider,
+    CardBody,
+} from "@nextui-org/react";
+import React from "react";
+
+export default function ChatPage() {
+    return (
+        <>
+            <CardHeader className="text-2xl font-semibold text-default">
+                Chat
+            </CardHeader>
+            <Divider />
+            <CardBody>Chat goes here</CardBody>
+        </>
+    );
+}
